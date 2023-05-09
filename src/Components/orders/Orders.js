be5c98305f407ed76a829725b095e123c3cb9885
Bolade Adeyemi <boladeadeyemi@gmail.com>
2023-05-09 12:07:03 +0100

    Third commit

--- src/Components/orders/Orders.js
+++ src/Components/orders/Orders.js
@@ -2,17 +2,24 @@ import React from 'react'
 import './Orders.css'
 import { getBasketTotal } from '../../reducer'
 import { useStateValue } from '../../StateProvide';
-import { Link } from 'react-router-dom';
+import {useHistory } from 'react-router-dom';
+
 
 
 
 function Orders() {
+    const history = useHistory();
     const [{basket, user}, dispatch ] = useStateValue();
+    const toHome = () => {
+      history.push("/home")
+      window.location.reload()
+    }; 
+
   return (
     <div className='orders'>
        <h1>Your Orders Report</h1> 
         <p>{user?.email}, your Order of ${getBasketTotal(basket)} is successfull. </p>
-        <Link to = '/home'><button>Go back to the Home Page</button></Link>
+        <button onClick={toHome}>Go back to the Home Page</button>
         
     </div>
   )
