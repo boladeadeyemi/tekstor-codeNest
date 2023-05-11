93447454071cb48ca46f9ed210db7154aef3fbf6
Bolade Adeyemi <boladeadeyemi@gmail.com>
2023-05-11 15:11:57 +0100

    Another amazing commit

--- src/Components/product/Product.js
+++ src/Components/product/Product.js
@@ -2,20 +2,16 @@ import React from 'react'
 import './Product.css'
 import Itemdetails from '../itemdetails/Itemdetails'
 import { useStateValue } from '../../StateProvide'
-import Subtotal from '../subtotal/Subtotal'
+import { getBasketTotal } from '../../reducer';
+import { useNavigate } from 'react-router-dom';
+// import Subtotal from '../subtotal/Subtotal'
 
 function Product() {
   const [{basket,user}, dispatch] = useStateValue()
+  const navigate = useNavigate();
 
   return (
     <div className="product">
-            <div className="bg1__productpage">
-            </div>
-              {/* <div className="user__greeting">
-                  
-              </div> */}
-
-        <div>
         <div className="item__productpage">
         <p className="title"> ITEM DETAILS</p>
         <h3>These are your item details, {user?.email}. You have {basket?.length} items</h3>
@@ -33,14 +29,10 @@ function Product() {
         
         </div>
          
-        </div>
 
-
-  
-
-
-    <div className="checkout__right">
-           <Subtotal/>
+        <div className="checkout__right">
+            <p>Subtotal:  ${getBasketTotal(basket)}</p>
+            <button onClick={ () => navigate("/review")}>Proceed to Review</button>
         </div>
 
 
